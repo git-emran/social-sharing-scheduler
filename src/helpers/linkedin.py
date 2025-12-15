edb921b0e52424aa2937b8fa8a0656788112183a
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-12-15 19:13:02 +0600

    better error handling

---
+++ src/helpers/linkedin.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import get_user_model
+import requests
+
+
+def get_linkedin_user_details(user):
+    try:
+        linkedin_social = user.socialaccount_set.get(provider="linkedin")
+    except:
+        raise Exception("linkedin is incorrect")
+    return linkedin_social
+
+
+def get_share_header(linkedin_social):
+    tokens = linkedin_social.socialtoken_set.all()
+    if not tokens.exists():
+        raise Exception("Linkedin Tokens are invalid")
+    social_token = tokens.first()
+    return {
+        "Authorization": f"Bearer {social_token.token}",
+        "X-Restli-Protocol-Version": "2.0.0",
+    }
+
+
+def post_to_linkedin(user, text: str):
+    User = get_user_model()
+    if not isinstance(user, User):
+        raise Exception("Must be a user")
+    linkedin_social = get_linkedin_user_details(user)
+    linkedin_user_id = linkedin_social.uid
+    if not linkedin_user_id:
+        raise Exception("Invalid linkedin user id")
+    headers = get_share_header(linkedin_social)
+    endpoint = "https://api.linkedin.com/v2/ugcPosts"
+    payload = {
+        "author": f"urn:li:person:{linkedin_user_id}",
+        "lifecycleState": "PUBLISHED",
+        "specificContent": {
+            "com.linkedin.ugc.ShareContent": {
+                "shareCommentary": {"text": f"{text}"},
+                "shareMediaCategory": "NONE",
+            }
+        },
+        "visibility": {"com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"},
+    }
+
+    response = requests.post(endpoint, json=payload, headers=headers)
+
+    try:
+        response.raise_for_status()
+    except Exception:
+        raise Exception("Invalid post, please try again later")
+    return response
